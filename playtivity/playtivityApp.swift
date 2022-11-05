1580201204de09800dddf01b2857df15d5b29ced
CSFatimaAJ <csfatimaaljaber@icloud.com>
2022-11-05 21:28:39 +0300

    Initial Commit

---
+++ playtivity/playtivityApp.swift
@@ -0,0 +1,17 @@
+//
+//  playtivityApp.swift
+//  playtivity
+//
+//  Created by Fatima Aljaber on 05/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct playtivityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
